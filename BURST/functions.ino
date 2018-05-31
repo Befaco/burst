f509c79cec96441453248cd1fb9cbcedc11dd918
Jeremy Bernstein <jeremy.d.bernstein@googlemail.com>
2018-05-31 13:27:03 +0200

    "silent" burst no longer displayed

--- BURST/functions.ino
+++ BURST/functions.ino
@@ -598,9 +598,9 @@ float mapfloat(float x, float inMin, float inMax, float outMin, float outMax)
 void handleLEDs(unsigned long now)
 {
   if ((now >= ledQuantityTime + 350)) {
-    if (wantsMoreBursts || (now >= ledParameterTime + 750)) {
+    if ((wantsMoreBursts && !silentBurst) || (now >= ledParameterTime + 750)) {
       for (int i = 0; i < 4; i++) {
-        digitalWrite(ledPin[i], bitRead(burstStarted ? repetitionCounter : repetitions_Temp - 1, i));
+        digitalWrite(ledPin[i], bitRead((burstStarted && !silentBurst) ? repetitionCounter : repetitions_Temp - 1, i));
       }
     }
   }
